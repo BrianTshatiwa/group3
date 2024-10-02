9bd4a4bf5b64d66c62661e4dc9af98d759a704b9
Kutlwano Tshatiwa <G23T1361@ict.ru.ac.za>
2024-10-02 13:43:26 +0200

    start a new gdx project
    upload gdx files

---
+++ android/src/main/java/com/kutlwanotshatiwa/flappybird/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.kutlwanotshatiwa.flappybird.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kutlwanotshatiwa.flappybird.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
